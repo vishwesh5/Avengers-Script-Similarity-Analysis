fdb694521c619db5a0631a0cf2e7feb664a060eb
vishwesh5 <vishweshshrimali5@gmail.com>
2018-11-13 12:02:52 +0530

    Add character lines

---
+++ get-character-lines.py
@@ -0,0 +1,28 @@
+chars = []
+file_handles = {}
+script = open("script-processed",'r')
+for line in script.readlines():
+    line.strip()
+    if line=="":
+        continue
+    else:
+        try:
+            if ":" not in line:
+                continue
+            character = line.split(":")[0]
+            if len(character.split())>2:
+                print(line,character)
+                input()
+            #print(line,character)
+            #input()
+            if character=="":
+                continue
+            elif character in chars:
+                file_handles[character].write("{}\n".format(line))
+            else:
+                chars.append(character)
+                file_handles[character] = open(character+".txt",'w')
+                file_handles[character].write("{}\n".format(line))
+        except:
+            print("Unknown error. Skipping line...")
+#print("Files written for:\n{}".format("\n".join(chars)))
